97f3888f9e6d4ce55c330c74f747ac759725cfde
Kino1994 <joaquindevicente@hotmail.es>
2024-06-09 16:13:01 +0200

    fix fred data

--- fed.py
+++ fed.py
@@ -1,5 +1,6 @@
 from urllib.parse import urlencode
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def get_fred_data(**kwargs):
@@ -18,4 +19,18 @@ df.columns = ["Date",
               "Federal Funds Target Range - Lower Limit",
               "Federal Funds Target Rate"]
 df["Date"] = pd.to_datetime(df["Date"], format='%Y-%m-%d')
+df.loc[df['Date'] == '2008-12-16', 'Federal Funds Target Rate'] = df.loc[df['Date'] == '2008-12-16', 'Federal Funds Target Range - Upper Limit']
 df.iloc[:, 1:] = df.iloc[:, 1:].apply(pd.to_numeric, axis=1, errors='coerce')
+
+plt.figure(figsize=(14, 8))
+plt.step(df['Date'], df['Federal Funds Target Rate'], label='Federal Funds Target Rate', where='post')
+plt.step(df['Date'], df['Federal Funds Target Range - Upper Limit'], label='Federal Funds Target Range - Upper Limit', linestyle='--',
+         where='post')
+plt.step(df['Date'], df['Federal Funds Target Range - Lower Limit'], label='Federal Funds Target Range - Lower Limit', linestyle=':', where='post')
+
+plt.xlabel('Date')
+plt.ylabel('Rate')
+plt.title('FED Interest Rates Over Time')
+plt.legend()
+plt.grid(True)
+plt.savefig("fed/fed.png", dpi=1200, bbox_inches='tight')
